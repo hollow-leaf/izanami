30d828321d915f187e66a86d8b9f694329dbc767
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2024-09-22 02:44:52 +0800

    feat: url param

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,18 +2,22 @@ import './App.css'
 import { Header } from './component/header'
 import { AppKitProvider } from './AppKitProvider'
 import { AddrStatusList } from './component/addrStatusList'
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <AppKitProvider>
-      <main className="w-full bg-white flex flex-col items-center">
-        <Header />
-        <div className='w-[1200px]'>
-          <AddrStatusList/>
-        </div>
-      </main>
-    </AppKitProvider>
+    <Router>
+      <AppKitProvider>
+        <main className="w-full bg-white flex flex-col items-center">
+          <Header />
+          <div className='w-[1200px]'>
+            <AddrStatusList/>
+          </div>
+        </main>
+      </AppKitProvider>
+    </Router>
   )
 }
 
